650fdb5c8890bd972caf67d18b73458476cdf21e
alighareeb01 <ghareeb874@gmail.com>
2026-04-25 15:55:55 +0300

    fixing for vervel

--- src/index.js
+++ src/index.js
@@ -3,14 +3,7 @@ dotenv.config({ path: "config/.env" });
 
 import app from "../src/app.js";
 
-app.use(async (req, res, next) => {
-  try {
-    await databaseConnection();
-    next();
-  } catch (err) {
-    next(err);
-  }
-});
+
 
 
 export default app;
